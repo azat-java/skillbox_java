3db4e8b99b89286528b593ead1ab35b47210c71d
idrisovazat <idrisovazat@gmail.com>
2020-02-16 02:25:56 +0300

    HW 4.6 done

---
+++ 4/4.6/Birthday/src/Main.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        final LocalDate birthday = LocalDate.of(1996, 2, 2);
+        LocalDate nextBirthday = birthday;
+        LocalDate now = LocalDate.now();
+        int years = Period.between(birthday, now).getYears();
+        Locale localeRu = new Locale("ru", "RU");
+        for (int i = 0; i <= years; i++) {
+            System.out.println(i + " - " + nextBirthday.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")) + " - " + nextBirthday.getDayOfWeek().getDisplayName(TextStyle.FULL, localeRu));
+            nextBirthday = nextBirthday.plusYears(1);
+        }
+    }
+}
