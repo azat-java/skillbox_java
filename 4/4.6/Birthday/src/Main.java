40dea5ffe5083d5a4c87f40a50e63fda0afdee9a
idrisovazat <idrisovazat@gmail.com>
2020-03-12 23:39:54 +0300

    HW 4.6 fixes

--- 4/4.6/Birthday/src/Main.java
+++ 4/4.6/Birthday/src/Main.java
@@ -1,22 +1,16 @@
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.format.DateTimeFormatter;
-import java.time.format.TextStyle;
 import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        final LocalDate birthday = LocalDate.of(1996, 2, 2);
-        LocalDate nextBirthday = birthday;
+        LocalDate birthday = LocalDate.of(1996, 2, 2);
         LocalDate now = LocalDate.now();
-        int years = Period.between(birthday, now).getYears();
-        Locale localeRu = new Locale("ru", "RU");
-        for (int i = 0; i <= years; i++) {
-            System.out.println(i + " - " + nextBirthday.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")) + " - " + nextBirthday.getDayOfWeek().getDisplayName(TextStyle.FULL, localeRu));
-            nextBirthday = nextBirthday.plusYears(1);
+        int i = 0;
+        while (now.isAfter(birthday)) {
+            System.out.println(i + " - " + birthday.format(DateTimeFormatter.ofPattern("dd.MM.yyyy - eeee", new Locale("ru"))));
+            birthday = birthday.plusYears(1);
+            i++;
         }
-        int y = 9;
-        int o = y++;
-        System.out.println(y + " " + o);
     }
 }
