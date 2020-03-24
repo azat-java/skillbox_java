42ddb36dfbd349a934c87425792eed02e11e0848
idrisovazat <idrisovazat@gmail.com>
2020-03-24 16:55:11 +0300

    HW 5.1 fixes

--- 05_ArraysAndCollections/Rainbow/src/Main.java
+++ 05_ArraysAndCollections/Rainbow/src/Main.java
@@ -2,11 +2,12 @@ public class Main {
     public static void main(String[] args) {
         String text = "Каждый охотник желает знать, где сидит фазан";
         String[] colors = text.split(",?\\s+");
-        String[] colorsInverted = new String[colors.length];
-        for (int i = colors.length - 1; i >= 0; i--) {
-            colorsInverted[colors.length - 1 - i] = colors[i];
+        String temp;
+        for (int i = 0; i <= colors.length / 2; i++) {
+            temp = colors[i];
+            colors[i] = colors[colors.length - 1 - i];
+            colors[colors.length - 1 - i] = temp;
         }
-        colors = colorsInverted;
         for (int i = 0; i < colors.length; i++) {
             System.out.println(colors[i]);
         }
--- 05_ArraysAndCollections/Temperatures/src/Main.java
+++ 05_ArraysAndCollections/Temperatures/src/Main.java
@@ -1,23 +1,29 @@
+import java.text.DecimalFormat;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
-        double[] temperatures = new double[30];
+        final int PATIENTS = 30;
+        final double MAX_TEMP = 40.0;
+        final double MIN_TEMP = 32.0;
+        final double MIN_TEMP_WELL = 36.2;
+        final double MAX_TEMP_WELL = 36.9;
+        double[] temperatures = new double[PATIENTS];
         double sum = 0.;
         int healthyPatients = 0;
+        System.out.println("Температура пациентов: ");
         for (int i = 0; i < temperatures.length; i++) {
-            temperatures[i] = Math.round(ThreadLocalRandom.current().nextDouble(32, 40) * 10) / 10.;
+            temperatures[i] = ThreadLocalRandom.current().nextDouble(MIN_TEMP, MAX_TEMP);
+            DecimalFormat formatter = new DecimalFormat("#0.00°C");
+            System.out.println(formatter.format(temperatures[i]));
             sum += temperatures[i];
-            if (temperatures[i] >= 36.2 && temperatures[i] <= 36.9) {
+            if (temperatures[i] >= MIN_TEMP_WELL && temperatures[i] <= MAX_TEMP_WELL) {
                 healthyPatients++;
             }
         }
-        System.out.println("Температура пациентов: ");
-        for (double temperature : temperatures) {
-            System.out.print(temperature + ", ");
-        }
-        System.out.println();
-        System.out.println("Средняя температура по больнице: " + Math.round(sum / 30) * 100 / 100.);
+        double averageTemperature = sum / PATIENTS;
+
+        System.out.printf("Средняя температура = %.02f°C \n", averageTemperature);
         System.out.println("Количество здоровых пациентов: " + healthyPatients);
     }
 }
