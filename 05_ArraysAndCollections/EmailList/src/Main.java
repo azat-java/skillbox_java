76ebcb862d3aef7800f4b5450fc98ca87ddb99a7
idrisovazat <idrisovazat@gmail.com>
2020-03-26 14:41:26 +0300

    HW 5.3 fix

--- 05_ArraysAndCollections/EmailList/src/Main.java
+++ 05_ArraysAndCollections/EmailList/src/Main.java
@@ -4,30 +4,45 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+    final static String ADD = "ADD";
+    final static String LIST = "LIST";
+    static HashSet<String> emailList = new HashSet<>();
+    final static String REG_EMAIL = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+
     public static void main(String[] args) {
-        HashSet<String> emailList = new HashSet<>();
         while (true) {
             System.out.println("Введите команду: ");
             Scanner scanner = new Scanner(System.in);
             String command = scanner.nextLine().trim();
             String commands[] = command.split("\\s+");
 
-            if (commands[0].equals("LIST")) {
-                for (String email : emailList) {
-                    System.out.println(email);
-                }
-            } else if (commands[0].equals("ADD") && commands.length == 2) {
-                String regex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(commands[1]);
-                if (matcher.matches()) {
-                    emailList.add(commands[1]);
-                } else {
-                    System.out.println("Некорректный ввод");
-                }
+            if (commands[0].equals(LIST)) {
+                printList();
+            } else if (commands[0].equals(ADD) && commands.length == 2) {
+                add(commands[1]);
             } else {
-                System.out.println("Некорректный ввод");
+                error();
             }
         }
+    }
+
+    static void printList() {
+        for (String email : emailList) {
+            System.out.println(email);
+        }
+    }
+
+    static void add(String email) {
+        Pattern pattern = Pattern.compile(REG_EMAIL);
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.matches()) {
+            emailList.add(email);
+        } else {
+            error();
+        }
+    }
+
+    static void error() {
+        System.out.println("Некорректный ввод");
     }
 }
